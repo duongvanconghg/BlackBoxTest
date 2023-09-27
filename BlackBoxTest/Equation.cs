17d4e4fb3f693edd151219f53dc1dd3157f867e2
duongvanconghg <duongvanconghg@gmail.com>
2023-09-28 02:28:24 +0700

    Add project files.

---
+++ BlackBoxTest/Equation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoxTest
+{
+    public class Equation
+    {
+        public static double Solve(double a, double b)
+        {
+            if (a == 0)
+            {
+                // Phương trình vô nghiệm hoặc có vô số nghiệm
+                if (b == 0)
+                {
+                    return double.PositiveInfinity; // Vô số nghiệm
+                }
+                else
+                {
+                    return double.NegativeInfinity; // Vô nghiệm
+                }
+            }
+            else
+            {
+                // Phương trình có nghiệm duy nhất
+                return -b / a;
+            }
+        }
+    }
+}
+
---
+++ TestCase/UnitTest.cs
@@ -0,0 +1,40 @@
+﻿
+using BlackBoxTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCase
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [TestCase(2, -4, 2.0)] // Phương trình có nghiệm duy nhất
+        [TestCase(1, 0, 0)]// Phương trình có nghiệm duy nhất x = 0
+        [TestCase(0, 4, double.NegativeInfinity)] // Phương trình vô nghiệm
+        [TestCase(0, 0, double.PositiveInfinity)] // Phương trình có vô số nghiệm
+
+
+        [TestCase(double.NegativeInfinity, 4, 0.0)] // Trường hợp 1: a tiến đến âm vô cùng
+        [TestCase(-0.01, 4, 400.0)] // Trường hợp 2: a tiến đến giá trị âm nhỏ
+        [TestCase(0.0, 4, double.NegativeInfinity)] // Trường hợp 3: a bằng 0
+        [TestCase(0.01, 4, -400.0)] // Trường hợp 4: a tiến đến giá trị dương nhỏ
+        [TestCase(double.PositiveInfinity, 4, -0.0)] // Trường hợp 5: a tiến đến dương vô cùng
+
+
+        [TestCase(2, double.NegativeInfinity, double.PositiveInfinity)] // Trường hợp 6: b tiến đến âm vô cùng
+        [TestCase(2, -0.01, 0.005)] // Trường hợp 7: b tiến đến giá trị âm nhỏ
+        [TestCase(2, 0.0, 0.0)] // Trường hợp 8: b bằng 0
+        [TestCase(2, 0.01, -0.005)] // Trường hợp 9: b tiến đến giá trị dương nhỏ
+        [TestCase(2, double.PositiveInfinity, double.NegativeInfinity)] // Trường hợp 10: b tiến đến dương vô cùng
+
+        public void TestSolve(double a, double b, double expected)
+        {
+            double result = Equation.Solve(a, b);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
